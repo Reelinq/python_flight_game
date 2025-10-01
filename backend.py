f8903a4ecbad3084eeed8ce6b4175e63a77729d5
Reelinq <savbopa@gmail.com>
2025-10-01 07:42:54 +0300

    Created a database for the game and ensured that it works. Added requiremtns with the sql-connector library

--- backend.py
+++ backend.py
@@ -1,4 +1,21 @@
+import mysql.connector
+from mysql.connector import Error
+
 SETTINGS = {
 	"initial_co2_budget": 2000,
 	"co2_per_100km": 20
 }
+
+def get_connection():
+	return mysql.connector.connect(
+	host="localhost",
+	user="root",
+	password="",
+	database="flight_game"
+)
+
+if __name__ == "__main__":
+	conn = get_connection()
+	if conn:
+		print("✅ Connected to database!")
+		conn.close()
